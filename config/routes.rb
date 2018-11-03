f479c5268ad04be1d7135c8c0de9a40e51271133
toticavalcanti <toticavalcanti@hotmail.com>
2018-11-03 12:33:56 -0300

    update MyTwitter/config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  post 'user_token' => 'user_token#create'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ 
+  namespace :api do
+    namespace :v1 do
+      post 'user_token', to: 'user_token#create'
+    end
+  end
 end
