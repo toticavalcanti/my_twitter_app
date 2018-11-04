6f1b9fa4031e7ae78c1381a279b96a8b88708476
toticavalcanti <toticavalcanti@hotmail.com>
2018-11-04 09:35:38 -0200

    update MyTwitter/app/models/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
-class User < ApplicationRecord
+class User < ApplicationRecord #>
     has_secure_password
-end
+    validates_length_of :password,
+                        maximum: 72,
+                        minimum: 8,
+                        allow_nil: true,
+                        allow_blank: false
+  
+    validates_presence_of :name, :email
+  
+  
+    acts_as_voter
+    acts_as_followable
+    acts_as_follower
+    has_many :tweets, dependent: :destroy
+  end
