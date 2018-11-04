03204e84fb1b76e70319f9f3362a07b7f49f4cc5
toticavalcanti <toticavalcanti@hotmail.com>
2018-11-04 10:44:13 -0200

    update MyTwitter/app/controllers/api/v1/api_controller.rb

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,8 +1,10 @@
 module Api
-    module V1
-      class ApiController < ApplicationController
-        include Knock::Authenticable
-        #> Métodos globais
-      end
+  module V1
+    class ApiController < ApplicationController
+      include Knock::Authenticable
+      include CanCan::ControllerAdditions
+    
+      #> Métodos globais
     end
   end
+end
