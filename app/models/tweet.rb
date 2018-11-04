3aca825b61f442b1bbc55409f10cf251c715eaca
toticavalcanti <toticavalcanti@hotmail.com>
2018-11-04 09:53:44 -0200

    update tweet model

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,7 @@
-class Tweet < ApplicationRecord
+class Tweet < ApplicationRecord # >
   belongs_to :user
+  acts_as_votable
+  belongs_to :tweet_original, class_name: 'Tweet', required: false
+  has_many :retweets, class_name: 'Tweet', foreign_key: 'tweet_original_id'
+  validates_presence_of :body, :user_id
 end
