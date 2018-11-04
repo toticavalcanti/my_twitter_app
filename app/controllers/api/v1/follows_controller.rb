af74222424f929b5d488e66c57885c0876ff4719
toticavalcanti <toticavalcanti@hotmail.com>
2018-11-04 12:21:48 -0200

    generate follows controller

---
+++ app/controllers/api/v1/follows_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::FollowsController < ApplicationController
+  def create
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get 'follows/create'
+      get 'follows/destroy'
+    end
+  end
+  namespace :api do
+    namespace :v1 do
       get 'likes/create'
       get 'likes/destroy'
     end
