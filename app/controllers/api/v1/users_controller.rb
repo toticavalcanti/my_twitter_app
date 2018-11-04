9d6840a2ee2375e7ac5ff2ebc31a2d3e231c033b
toticavalcanti <toticavalcanti@hotmail.com>
2018-11-04 12:00:43 -0200

    generate user controller

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::UsersController < ApplicationController
+  def create
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
+  def current
+  end
+
+  def show
+  end
+
+  def following
+  end
+
+  def followers
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get 'users/create'
+      get 'users/destroy'
+      get 'users/update'
+      get 'users/current'
+      get 'users/show'
+      get 'users/following'
+      get 'users/followers'
+    end
+  end
+  namespace :api do
+    namespace :v1 do
       post 'user_token', to: 'user_token#create'
     end
   end
