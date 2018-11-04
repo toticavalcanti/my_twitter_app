0c48a814ad182abba587c4794e2f8026607dfb1d
toticavalcanti <toticavalcanti@hotmail.com>
2018-11-04 12:28:51 -0200

    update MyTwitter/config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,17 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'follows/create'
-      get 'follows/destroy'
-    end
-  end
-  namespace :api do
-    namespace :v1 do
-      get 'likes/create'
-      get 'likes/destroy'
-    end
-  end
-  namespace :api do
-    namespace :v1 do
-      get 'tweets/index'
-      get 'tweets/show'
-      get 'tweets/create'
-      get 'tweets/destroy'
-      get 'tweets/update'
-    end
-  end
-  namespace :api do
-    namespace :v1 do
-      get 'users/create'
-      get 'users/destroy'
-      get 'users/update'
-      get 'users/current'
-      get 'users/show'
-      get 'users/following'
-      get 'users/followers'
-    end
-  end
-  namespace :api do
-    namespace :v1 do
       post 'user_token', to: 'user_token#create'
+      resources :follows, only: %i[create destroy]
+      resources :likes, only: %i[create destroy]
+      resources :tweets, only: %i[index show create update destroy]
+      resources :users, only: %i[show create update destroy] do
+        member do
+          get 'following'
+          get 'followers'
+        end
+        get 'current', on: :collection
+      end
     end
   end
 end
