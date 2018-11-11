e138d20d07ffd6881c2b57b0f7bc5e5d439a481a
toticavalcanti <toticavalcanti@hotmail.com>
2018-11-11 14:20:26 -0200

    set app/serializers/api/v1/tweet_serializer.rb

--- app/serializers/api/v1/tweet_serializer.rb
+++ app/serializers/api/v1/tweet_serializer.rb
@@ -1,3 +1,31 @@
-class Api::V1::TweetSerializer < ActiveModel::Serializer
-  attributes :id
+module Api
+  module V1
+    class TweetSerializer < ActiveModel::Serializer # >
+      attributes :id, :body, :tweet_original_id, :retweets_count, :likes_count, :liked
+      belongs_to :tweet_original
+      belongs_to :user
+ 
+      def tweet_original
+        options = { each_serializer: Api::V1::TweetSerializer }
+        ActiveModelSerializers::SerializableResource.new(object.tweet_original, options) if object.tweet_original
+      end
+ 
+      def user
+        options = { each_serializer: Api::V1::UserSerializer }
+        ActiveModelSerializers::SerializableResource.new(object.user, options)
+      end
+ 
+      def retweets_count
+        object.retweets.count
+      end
+ 
+      def likes_count
+        object.votes_for.size
+      end
+ 
+      def liked
+        object.liked_by current_user
+      end
+    end
+  end
 end
