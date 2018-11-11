aff051f7770440a076c22a8a6af61d95dd243898
toticavalcanti <toticavalcanti@hotmail.com>
2018-11-11 13:46:55 -0200

    set app/serializers/api/v1/user_serializer.rb

--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -1,3 +1,19 @@
-class Api::V1::UserSerializer < ActiveModel::Serializer
-  attributes :id
+module Api
+  module V1
+    class UserSerializer < ActiveModel::Serializer # >
+      attributes :id, :name, :email, :tweets_count, :followers_count, :following_count
+ 
+      def tweets_count
+        object.tweets.count
+      end
+ 
+      def followers_count
+        object.followers_by_type('User').count
+      end
+ 
+      def following_count
+        object.following_users.count
+      end
+    end
+  end
 end
